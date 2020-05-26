d7245aaaadb36162fd8753f9bb240faa4c8a764b
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2020-05-26 19:10:29 -0300

    - ShObject methods

--- src/shio/ShObject.ts
+++ src/shio/ShObject.ts
@@ -2,10 +2,30 @@
 
 export class ShObject {
     public constructor() {
-    
+
     }
 
-    public navigation(siteName: string, isHome: boolean): Array<any> {
+    public formComponent(shPostTypeName: string, shObjectId: string): string {
+        return null;
+    }
+
+	/**
+	 * @desc Returns Search Result
+	 * @public
+	 */
+    public searchComponent(): string {
+        return null;
+    }
+
+	/**
+	 * @desc Returns Folder Navigation Component
+	 * @param siteName
+	 *            Site Name.
+	 * @param home
+	 *            true or false to show the Home folder.
+	 * @public
+	 */
+    public navigation(siteName: string, home: boolean): Array<any> {
         return [
             {
                 "id": "123",
@@ -16,9 +36,135 @@ export class ShObject {
                 "name": "Folder2",
             }
         ];
-    };
+    }
 
-    public generateFolderLink = function (folderId: string): string {
+	/**
+	 * @desc Returns Folder Navigation Component from Parent Folder
+	 * @param folderId
+	 *            Folder Id.
+	 * @param home
+	 *            true or false to show the Home folder.
+	 * @public
+	 */
+    public navigationFolder(folderId: string, home: boolean): string {
+        return null;
+    }
+
+	/**
+	 * @desc Returns Query Component
+	 * @param folderId
+	 *            Folder Id.
+	 * @param postTypeName
+	 *            Post Type Name.
+	 * @public
+	 */
+
+    public query(folderId: string, postTypeName: boolean): string {
+        return null;
+    }
+
+	/**
+	 * @desc Returns Query Component
+	 * @param postTypeName
+	 *            Post Type Name.         
+	 * @public
+	 */
+    public queryByPostType(postTypeName: string): string {
+        return null;
+    }
+
+	/**
+	 * @desc Returns Query Component
+	 * @param postTypeName
+	 *            Post Type Name.
+	 * @param postAttrName
+	 *            Post Type Attribute Name.
+	 * @param arrayValue
+	 *            Array Value.            
+	 * @public
+	 */
+    public queryByPostTypeIn(postTypeName: string, postAttrName: string, arrayValue: string): string {
+        return null;
+    }
+
+	/**
+	 * @desc Returns getRelation Component
+	 * @param shPostAttrId,
+	 *            Post Attribute Id.
+	 * @public
+	 */
+    public getRelation(shPostAttrId: string): string {
+        return null;
+    }
+
+	/**
+	 * @desc Generate Post Link
+	 * @param postId
+	 *            Post Id.
+	 * @public
+	 */
+    public generatePostLink(postId: string): string {
+        return null;
+    }
+
+	/**
+	 * @desc Generate Folder Link
+	 * @param folderId
+	 *            Folder Id.
+	 * @public
+	 */
+    public generateFolderLink(folderId: string): string {
         return "link123";
-    };
+    }
+
+	/**
+	 * @desc Get Post Map
+	 * @param postId
+	 *            Post Id.
+	 * @public
+	 */
+    public getPost(postId: string): string {
+        return null;
+    }
+
+	/**
+	 * @desc Get Folder Map
+	 * @param folderId,
+	 *            Folder Id.
+	 * @public
+	 */
+    public getFolderMap(folderId: string): string {
+        return null;
+    }
+
+	/**
+	 * @desc Get Parent Folder Map
+	 * @param folderId
+	 *            Folder Id.
+	 * @public
+	 */
+    public getParentFolder(folderId: string): string {
+        return null;
+    }
+	/**
+	 * @desc Generate Object Link
+	 * @param objectId
+	 *            Object Id.
+	 * @public
+	 */
+    public generateObjectLink(objectId: string): string {
+        return null;
+    }
+
+	/**
+	 * @desc Generate Image Link
+	 * @param objectId
+	 *            Object Id.
+	 * @param scale
+	 *            scale.
+	 * @public
+	 */
+    public generateImageLink(objectId: string, scale: string): string {
+        return null;
+    }
 }
--- src/shio/content/ShContentSite.ts
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import { ShContentSiteSystem } from "./ShContentSiteSystem";
-
-export class ShContentSite {
-
-    public system: ShContentSiteSystem;
-
-}
